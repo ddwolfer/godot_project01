2493ce68c5fe3793e05f51209910cc1dc69219b7
ddwolf <910063@gmail.com>
2023-10-14 13:40:34 +0800

    update

--- Script/character/character.gd
+++ Script/character/character.gd
@@ -9,6 +9,14 @@ var isMove:bool = true
 @onready var m_bodyNode = get_node("Body")
 @onready var m_bodyAni = get_node("Body/AnimatedSprite2D")
 
+@onready var m_clothesTopNode = get_node("clothes/Top")
+@onready var m_clothesTopAni = get_node("clothes/Top/AnimatedSprite2D")
+
+enum DIRECT {
+	left = 1 ,
+	right = 2
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -16,6 +24,22 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	PlayerMove(_delta)
+
+func AllNodeSetDirect(direct):
+	var scaleX = 1.0 if direct==DIRECT.right else -1.0
+
+	m_bodyNode.scale = Vector2(scaleX ,1.0)
+	m_clothesTopNode.scale = Vector2(scaleX ,1.0)
+
+func PlayAllAni( aniName ):
+	m_bodyAni.play(aniName)
+	m_clothesTopAni.play(aniName)
+
+func ResetAllAni():
+	m_bodyAni.stop()
+	m_bodyAni.frame = 0
+	m_clothesTopAni.stop()
+	m_clothesTopAni.frame = 0
 
 func PlayerMove(_delta):
 	print(_delta)
@@ -44,17 +68,18 @@ func PlayerMove(_delta):
 	if moveX != 0 or moveY != 0:
 		# 動畫更換
 		isMove = true
-		m_bodyAni.play(playAni)
+		PlayAllAni(playAni)
 		# 換方向
 		if moveX > 0:
-			m_bodyNode.scale = Vector2(1.0 ,1.0)
+			AllNodeSetDirect(DIRECT.right)
+			
 		elif moveX < 0:
-			m_bodyNode.scale = Vector2(-1.0 ,1.0)
+			AllNodeSetDirect(DIRECT.left)
+
 		# 移動
 		m_root.position.x = m_root.position.x + (moveX * moveSpeed * _delta)
 		m_root.position.y = m_root.position.y + (moveY * moveSpeed * _delta)
 	# 不動
 	elif isMove:
 		isMove = false
-		m_bodyAni.stop()
-		m_bodyAni.frame = 0
+		ResetAllAni()
